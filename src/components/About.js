9acec0d5b82d7a50ff4fad8e04ac2cff85b60c05
Tarun Kumar <tarun.kumar.csbs25@heritageit.edu.in>
2023-12-14 10:57:32 +0530

    first commit

--- src/components/About.js
+++ src/components/About.js
@@ -2,10 +2,7 @@ import React from 'react'
 
 export default function About(props) {
 
-    // const [myStyle, setMyStyle] = useState({
-    //     color: 'black',
-    //     backgroundColor: 'white'
-    // }) 
+  
     let myStyle = {
         color: props.mode ==='dark'?'white':'#042743',
         backgroundColor: props.mode ==='dark'?'rgb(36 74 104)':'white', 
@@ -23,7 +20,7 @@ export default function About(props) {
                     </h2>
                     <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
                     <div className="accordion-body" style={myStyle}>
-                    Textutils gives you a way to analyze your text quickly and efficiently. Be it word count, character count or 
+                    TextEditor gives you a way to analyze your text quickly and efficiently. Be it word count, character count or 
 
                     </div>
                     </div>
@@ -36,7 +33,7 @@ export default function About(props) {
                     </h2>
                     <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
                     <div className="accordion-body" style={myStyle}>
-                    TextUtils is a free character counter tool that provides instant character count & word count statistics for a given text. TextUtils reports the number of words and characters. Thus it is suitable for writing text with word/ character limit.
+                    TextEditor is a free character counter tool that provides instant character count & word count statistics for a given text. TextEditor reports the number of words and characters. Thus it is suitable for writing text with word/ character limit.
                     </div>
                     </div>
                 </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-// import { Link } from 'react-router-dom';
 
 export default function Navbar(props) {
+    
     return (
-        <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
-            <div className="container-fluid">
-                <a className="navbar-brand" href="/">{props.title}</a>
-                {/* <Link className="navbar-brand" to="/">{props.title}</Link> */}
+        <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode} ` }>
+            <div className="container-fluid " >
+                <a className="navbar-brand" href="/">TextEditor</a>
+                
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
@@ -15,11 +15,11 @@ export default function Navbar(props) {
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
                             <a className="nav-link" aria-current="page" href="/">Home</a>
-                            {/* <Link className="nav-link" aria-current="page" to="/">Home</Link> */}
+                           
                         </li>
                         <li className="nav-item">
                             <a className="nav-link" href="/about">{props.aboutText}</a>
-                            {/* <Link className="nav-link" to="/about">{props.aboutText}</Link> */}
+                            
                         </li>
                     </ul>
                     <div className={`form-check form-switch text-${props.mode==='light'?'dark':'light'}`}>
